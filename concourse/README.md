4fd4d8740902fe8f576022479da03940d7fb4661
Francisco Guerrero <aguerrero@pivotal.io>
2018-09-17 15:05:04 -0700

    Initial commit for PXF performance pipeline
    Co-authored-by: Francisco Guerrero <aguerrero@pivotal.io>
    Co-authored-by: Lav Jain <ljain@pivotal.io>
    
    [#160526219]

--- concourse/README.md
+++ concourse/README.md
@@ -47,6 +47,18 @@ fly -t ud set-pipeline -p pxf_pr \
     -v icw_green_bucket=gpdb5-assert-concourse-builds
 ```
 
+# Deploy the performance pipeline
+
+```
+fly -t ud set-pipeline -p pxf_perf -c ./pxf-perf-multi-node.yml \
+    -l ~/workspace/continuous-integration/secrets/gpdb_common-ci-secrets.yml \
+    -l ~/workspace/continuous-integration/secrets/gpdb_master-ci-secrets.yml \
+    -l pxf-multinode-params.yml \
+    -l ~/workspace/continuous-integration/secrets/pxf-secrets.yml \
+    -l ~/workspace/continuous-integration/secrets/ccp_ci_secrets_ud.yml \
+    -v gpdb-branch=master -v icw_green_bucket=gpdb5-assert-concourse-builds
+```
+
 # Deploy development PXF pipelines
 Dev pipelines can be deployed with an optional feature name
 ```
---
+++ concourse/scripts/pxf-perf-multi-node.bash
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+export PGHOST=mdw
+
+function create_database_and_schema {
+	psql <<EOF
+	DROP DATABASE IF EXISTS tpch;
+	CREATE DATABASE tpch;
+	\c tpch;
+	DROP TABLE IF EXISTS lineitem;
+	CREATE TABLE lineitem (
+		l_orderkey    INTEGER NOT NULL,
+		l_partkey     INTEGER NOT NULL,
+		l_suppkey     INTEGER NOT NULL,
+		l_linenumber  INTEGER NOT NULL,
+		l_quantity    DECIMAL(15,2) NOT NULL,
+		l_extendedprice  DECIMAL(15,2) NOT NULL,
+		l_discount    DECIMAL(15,2) NOT NULL,
+		l_tax         DECIMAL(15,2) NOT NULL,
+		l_returnflag  CHAR(1) NOT NULL,
+		l_linestatus  CHAR(1) NOT NULL,
+		l_shipdate    DATE NOT NULL,
+		l_commitdate  DATE NOT NULL,
+		l_receiptdate DATE NOT NULL,
+		l_shipinstruct CHAR(25) NOT NULL,
+		l_shipmode     CHAR(10) NOT NULL,
+		l_comment VARCHAR(44) NOT NULL
+	);
+EOF
+}
+
+function create_readable_external_table {
+	psql -c "CREATE PROTOCOL pxf"
+	psql -c "CREATE EXTERNAL TABLE hdfs_lineitem (like lineitem) LOCATION ('pxf://tmp/lineitem.tbl?PROFILE=HdfsTextSimple') FORMAT 'CSV'"
+}
+
+function write_data_from_external_to_gpdb {
+	psql -c "CREATE EXTERNAL TABLE hdfs_lineitem (like lineitem) LOCATION ('pxf://tmp/lineitem.tbl?PROFILE=HdfsTextSimple') FORMAT 'CSV'"
+}
+
+function main {
+	exit 1
+	create_database_and_schema
+	create_readable_external_table
+
+	time write_data_from_external_to_gpdb
+
+#	create_writable_external_table
+#	write_data_from_gpdb_to_external # time
+#
+#	validate_data
+}
+
