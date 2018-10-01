6a447a1889c1a25d03db1205bdcdfc1cb178f838
Ben Christel <bchristel@pivotal.io>
2018-10-01 14:03:38 -0700

    Use Centos 7 to use a recent jq version
    - Restore branch checking for the existence of pxf in components
      manifest
    
    Co-authored-by: Ben Christel <bchristel@pivotal.io>
    Co-authored-by: Francisco Guerrero <aguerrero@pivotal.io>

--- concourse/scripts/release_update_manifest.bash
+++ concourse/scripts/release_update_manifest.bash
@@ -8,10 +8,15 @@ yum -y install jq
 git clone --depth=1 gpdb_release gpdb_release_output
 cd gpdb_release_output
 
-cat components/component_manifest.json \
-  | jq "(.platforms[].components[] | select(.name==\"pxf\").version) = \"${VERSION}\"" \
-  > /tmp/component_manifest.json
-
+if [ "$(cat components/component_manifest.json | jq '.platforms[].components[] | select(.name=="pxf")')" == "" ]; then
+  cat components/component_manifest.json \
+	| jq ".platforms[].components += [{\"name\":\"pxf\",\"version\":\"${VERSION}\",\"bucket\":\"gpdb-stable-concourse-builds\",\"path\":\"components/pxf\",\"filetype\":\"tar.gz\",\"artifact_version\":\"latest\"}]" \
+	> /tmp/component_manifest.json
+else
+  cat components/component_manifest.json \
+    | jq "(.platforms[].components[] | select(.name==\"pxf\").version) = \"${VERSION}\"" \
+    > /tmp/component_manifest.json
+fi
 mv /tmp/component_manifest.json components/component_manifest.json
 
 git config user.email "pxf_bot@example.com"
