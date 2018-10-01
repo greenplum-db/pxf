36cc2bcd989167a49c814d044dc6f3d00978a8f3
Ben Christel <bchristel@pivotal.io>
2018-10-01 11:19:41 -0700

    Create task yml and script for gpdb_release update manifest task
    Co-authored-by: Ben Christel <bchristel@pivotal.io>
    Co-authored-by: Francisco Guerrero <aguerrero@pivotal.io>

---
+++ concourse/scripts/release_update_manifest.bash
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+pushd pxf_src
+VERSION=`git describe --tags`
+popd
+yum -y install jq
+git clone --depth=1 gpdb_release gpdb_release_output
+cd gpdb_release_output
+
+if [ "$(cat components/component_manifest.json | jq '.platforms[].components[] | select(.name=="pxf")')" == "" ]; then
+  cat components/component_manifest.json \
+	| jq ".platforms[].components += [{\"name\":\"pxf\",\"version\":\"${VERSION}\",\"bucket\":\"gpdb-stable-concourse-builds\",\"path\":\"components/pxf\",\"filetype\":\"tar.gz\",\"artifact_version\":\"latest\"}]" \
+	> /tmp/component_manifest.json
+else
+  cat components/component_manifest.json \
+	| jq "(.platforms[].components[] | select(.name==\"pxf\").version) = \"${VERSION}\"" \
+	> /tmp/component_manifest.json
+fi
+mv /tmp/component_manifest.json components/component_manifest.json
+
+git config user.email "pxf_bot@example.com"
+git config user.name "PXF_BOT"
+git commit -am "Update PXF manifest to version ${VERSION}"
