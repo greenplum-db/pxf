c4b2c536dc2d2535f0edcb08dc7368b8c6c7027e
Ben Christel <bchristel@pivotal.io>
2018-10-01 13:54:19 -0700

    Fix image for the update_manifest task
    - Assume component_manifest.json contains PXF sections
    
    Co-authored-by: Ben Christel <bchristel@pivotal.io>
    Co-authored-by: Francisco Guerrero <aguerrero@pivotal.io>

--- concourse/scripts/release_update_manifest.bash
+++ concourse/scripts/release_update_manifest.bash
@@ -8,15 +8,10 @@ yum -y install jq
 git clone --depth=1 gpdb_release gpdb_release_output
 cd gpdb_release_output
 
-if [ "$(cat components/component_manifest.json | jq '.platforms[].components[] | select(.name=="pxf")')" == "" ]; then
-  cat components/component_manifest.json \
-	| jq ".platforms[].components += [{\"name\":\"pxf\",\"version\":\"${VERSION}\",\"bucket\":\"gpdb-stable-concourse-builds\",\"path\":\"components/pxf\",\"filetype\":\"tar.gz\",\"artifact_version\":\"latest\"}]" \
-	> /tmp/component_manifest.json
-else
-  cat components/component_manifest.json \
-	| jq "(.platforms[].components[] | select(.name==\"pxf\").version) = \"${VERSION}\"" \
-	> /tmp/component_manifest.json
-fi
+cat components/component_manifest.json \
+  | jq "(.platforms[].components[] | select(.name==\"pxf\").version) = \"${VERSION}\"" \
+  > /tmp/component_manifest.json
+
 mv /tmp/component_manifest.json components/component_manifest.json
 
 git config user.email "pxf_bot@example.com"
