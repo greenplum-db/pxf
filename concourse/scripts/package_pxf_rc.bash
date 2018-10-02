f167934cd9d5d37acbb0d11a7177fc9146337184
Ben Christel <bchristel@pivotal.io>
2018-10-02 13:50:11 -0700

    squashme: debugging the packaging pipeline

--- concourse/scripts/package_pxf_rc.bash
+++ concourse/scripts/package_pxf_rc.bash
@@ -1,18 +1,15 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-set -x
+set -exo pipefail
 
 pushd pxf_src
 VERSION=`git describe --tags`
 popd
-cat > install_gpdb_component <<EOF
-#!/bin/sh
-set -x
-tar xzf pxf.tar.gz -C \$GPHOME
-EOF
+
+echo -e "#!/bin/bash\nset -x\ntar xzf pxf.tar.gz -C \$GPHOME" > install_gpdb_component
 chmod a+x install_gpdb_component
 cat > smoke_test_gpdb_component <<EOF
-#!/bin/sh
+#!/bin/bash
 set -x
 pwd
 ls
@@ -21,4 +18,5 @@ bin/pxf stop || exit 1
 EOF
 chmod a+x smoke_test_gpdb_component
 cp pxf_tarball/pxf.tar.gz .
-tar -cvzf pxf_artifacts/pxf-${VERSION}.tar.gz pxf.tar.gz install_gpdb_component smoke_test_gpdb_component
+touch sentinel-1
+tar -cvzf pxf_artifacts/pxf-${VERSION}.tar.gz pxf.tar.gz install_gpdb_component smoke_test_gpdb_component sentinel-1
