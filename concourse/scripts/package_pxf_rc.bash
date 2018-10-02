a612f52f88dee01f4b82a3d090bd3dfcd38846a6
Ben Christel <bchristel@pivotal.io>
2018-10-02 14:45:22 -0700

    Remove debugging code form package_pxf_rc.bash
    - Simplify commands

--- concourse/scripts/package_pxf_rc.bash
+++ concourse/scripts/package_pxf_rc.bash
@@ -6,17 +6,16 @@ pushd pxf_src
 VERSION=`git describe --tags`
 popd
 
-echo -e "#!/bin/bash\nset -x\ntar xzf pxf.tar.gz -C \$GPHOME" > install_gpdb_component
-chmod a+x install_gpdb_component
+cat > install_gpdb_component <<EOF
+#!/bin/bash
+set -x
+tar xzf pxf.tar.gz -C \$GPHOME
+EOF
 cat > smoke_test_gpdb_component <<EOF
 #!/bin/bash
 set -x
-pwd
-ls
-bin/pxf start &&
-bin/pxf stop || exit 1
+\$GPHOME/pxf/bin/pxf 2>&1 | grep "pxf {start|stop|restart|init|status}" || exit 1
 EOF
-chmod a+x smoke_test_gpdb_component
+chmod +x install_gpdb_component smoke_test_gpdb_component
 cp pxf_tarball/pxf.tar.gz .
-touch sentinel-1
-tar -cvzf pxf_artifacts/pxf-${VERSION}.tar.gz pxf.tar.gz install_gpdb_component smoke_test_gpdb_component sentinel-1
+tar -cvzf pxf_artifacts/pxf-${VERSION}.tar.gz pxf.tar.gz install_gpdb_component smoke_test_gpdb_component
