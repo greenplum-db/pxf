8d8efb39e07954e3e51e18c1b10323c08ace3dd6
Ben Christel <bchristel@pivotal.io>
2018-10-08 09:56:14 -0700

    Bundle PXF with only CentOS GPDB, not SLES
    This requires some heavy-duty jq magic to update the manifest
    file. It was tricky to get right, so I added tests.

---
+++ concourse/scripts/add_pxf_to_manifest.bash
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+function add_pxf_to_manifest() {
+    local version="$1"
+    local manifest="$2"
+    local filterfile=$(mktemp /tmp/jq_filter.XXXXXX)
+
+    cat > "$filterfile" <<EOF
+
+            # Declare some variables
+            . as \$root
+            | .platforms as \$platforms
+
+            # Get the name of each platform
+            | .platforms | keys
+
+            # At this point, the value in the pipeline looks like ["centos6", "centos7", "sles11"].
+            # We want to package PXF only with centos 6 and 7, so select only those platforms.
+            | map(select(. == "centos6" or . == "centos7"))
+
+            | reduce .[] as \$platform (
+
+                # Starting from the original \$platforms object...
+                \$platforms;
+
+                # Build up a new object by deeply merging with objects that have PXF in their
+                # components lists.
+                . + {
+                    (\$platform): (\$platforms."\\(\$platform)" + {
+                        components: (\$platforms."\\(\$platform)".components +
+                            [{
+                                "name":"pxf",
+                                "version":"${version}",
+                                "bucket":"gpdb-stable-concourse-builds",
+                                "path":"components/pxf",
+                                "filetype":"tar.gz",
+                                "artifact_version":"latest"
+                            }]
+                        )
+                    })
+                }
+            )
+
+            # Merge the new platforms object into the old \$root.
+            | \$root + {"platforms": . }
+EOF
+
+    cat "$manifest" | jq -f "$filterfile"
+    rm -f "$filterfile" >/dev/null
+}
