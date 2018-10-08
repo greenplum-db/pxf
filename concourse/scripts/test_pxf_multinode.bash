89eca1d0a6a934e282e647e8d72c6ff0bbbd084f
lavjain <ljain@pivotal.io>
2018-10-07 22:33:48 -0700

    Cleanup multi-cluster scripts

--- concourse/scripts/test_pxf_multinode.bash
+++ concourse/scripts/test_pxf_multinode.bash
@@ -13,19 +13,11 @@ function configure_local_hdfs() {
     sed -i -e "s/>tez/>mr/g" /etc/hive/conf/hive-site.xml
 }
 
-function ssh_access_to_hadoop() {
-
-    ssh ${SSH_OPTS} centos@hadoop "sudo mkdir -p /root/.ssh && \
-        sudo cp /home/centos/.ssh/authorized_keys /root/.ssh && \
-        sudo sed -i 's/PermitRootLogin no/PermitRootLogin yes/' /etc/ssh/sshd_config && \
-        sudo service sshd restart"
-}
-
 function run_multinode_smoke_test() {
 
     echo "Running multinode smoke test with ${NO_OF_FILES} files"
     time ssh hadoop "export JAVA_HOME=/etc/alternatives/jre_1.8.0_openjdk
-    /home/centos/singlecluster/bin/hdfs dfs -mkdir -p /tmp && mkdir -p /tmp/pxf_test && \
+    /singlecluster/bin/hdfs dfs -mkdir -p /tmp && mkdir -p /tmp/pxf_test && \
     for i in \$(seq 1 ${NO_OF_FILES}); do \
     cat > /tmp/pxf_test/test_\${i}.txt <<-EOF
 	1
@@ -33,8 +25,8 @@ function run_multinode_smoke_test() {
 	3
 	EOF
     done && \
-    /home/centos/singlecluster/bin/hdfs dfs -copyFromLocal /tmp/pxf_test/ /tmp && \
-    /home/centos/singlecluster/bin/hdfs dfs -chown -R gpadmin:gpadmin /tmp/pxf_test"
+    /singlecluster/bin/hdfs dfs -copyFromLocal /tmp/pxf_test/ /tmp && \
+    /singlecluster/bin/hdfs dfs -chown -R gpadmin:gpadmin /tmp/pxf_test"
 
     echo "Found $(hdfs dfs -ls /tmp/pxf_test | grep pxf_test | wc -l) items in /tmp/pxf_test"
     expected_output=$((3 * ${NO_OF_FILES}))
@@ -54,6 +46,7 @@ function run_multinode_smoke_test() {
 function open_ssh_tunnels() {
 
     # https://stackoverflow.com/questions/2241063/bash-script-to-setup-a-temporary-ssh-tunnel
+    ssh-keyscan hadoop >> /root/.ssh/known_hosts
     ssh -fNT -M -S /tmp/mdw5432 -L 5432:mdw:5432 gpadmin@mdw
     ssh -fNT -M -S /tmp/hadoop2181 -L 2181:hadoop:2181 root@hadoop
     ssh -S /tmp/mdw5432 -O check gpadmin@mdw
@@ -97,15 +90,11 @@ function _main() {
     install_gpdb_binary
     setup_gpadmin_user
     remote_access_to_gpdb
-    ssh_access_to_hadoop
 
     open_ssh_tunnels
     configure_local_hdfs
-
-    # Run multinode smoke test in parallel with pxf automation
-    run_multinode_smoke_test &
+    run_multinode_smoke_test
     run_pxf_automation
-    wait
     close_ssh_tunnels
 }
 
