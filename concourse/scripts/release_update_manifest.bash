0cabf7079286e9c6499117d1888f33ec609a14bc
Ben Christel <bchristel@pivotal.io>
2018-10-09 09:57:53 -0700

    Simplify JQ command to add PXF to GPDB's component manifest
    We'll now have to add PXF manually to the manifest for new
    platforms we want to support. The simplicity of the new jq
    command is probably worth this small additional manual effort.
    
    Co-authored-by: Lav Jain <ljain@pivotal.io>
    Co-authored-by: Ben Christel <bchristel@pivotal.io>

--- concourse/scripts/release_update_manifest.bash
+++ concourse/scripts/release_update_manifest.bash
@@ -4,17 +4,14 @@ set -euxo pipefail
 
 yum -y install git
 
-HERE=$(dirname $0)
-
-source "$HERE/add_pxf_to_manifest.bash"
-
 pushd pxf_src
 VERSION=`git describe --tags`
 popd
 git clone --depth=1 gpdb_release gpdb_release_output
 cd gpdb_release_output
 
-add_pxf_to_manifest "$VERSION" components/component_manifest.json \
+cat components/component_manifest.json \
+  | jq "(.platforms[].components[] | select(.name==\"pxf\").version) = \"${VERSION}\"" \
   > /tmp/component_manifest.json
 
 mv /tmp/component_manifest.json components/component_manifest.json
