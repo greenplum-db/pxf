b81f0e0dec63aa6b6ded742231e8a4e41184468f
Divya Bhargov <dbhargov@pivotal.io>
2018-10-24 16:50:36 -0700

    Install pxf-cluster CLI during server's `make install`
    To accomplish this, we moved the Go project for the CLI under the server
    directory.
    
    Co-authored-by: Divya Bhargov <dbhargov@pivotal.io>
    Co-authored-by: Ben Christel <bchristel@pivotal.io>

--- server/pxf-cli/go/src/pxf-cluster/main.go
+++ server/pxf-cli/go/src/pxf-cluster/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/greenplum-db/gp-common-go-libs/gplog"
 	"os"
 	"pxf-cluster/gpssh"
 	"pxf-cluster/greenplum"
@@ -9,8 +10,11 @@ import (
 
 func main() {
 	// TODO: validate args; this panics if no args are passed.
+	gplog.InitializeLogging("pxf", "")
+
 	remoteCommand := []string{"/usr/local/greenplum-db-devel/pxf/bin/pxf", os.Args[1]}
 	out, err := gpssh.Command(greenplum.GetSegmentHosts(), remoteCommand).CombinedOutput()
+
 	fmt.Println(string(out))
 	if err != nil {
 		fmt.Println(err)
