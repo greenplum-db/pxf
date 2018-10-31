689145303e46b01d52fe5dfbc9afc07ad15c50df
Ben Christel <bchristel@pivotal.io>
2018-10-31 13:27:41 -0700

    Run make install as root in CI
    gpadmin doesn't work because root owns the pxf_src directory.
    We have updated the docker image so root can run `go`, `dep`, and `ginkgo`.
    
    Co-authored-by: Oliver Albertini <oalbertini@pivotal.io>
    Co-authored-by: Ben Christel <bchristel@pivotal.io>

--- concourse/scripts/pxf_common.bash
+++ concourse/scripts/pxf_common.bash
@@ -148,17 +148,14 @@ function install_pxf_client() {
 }
 
 function install_pxf_server() {
-    # Relies on the working directory being /tmp/build/<sha>/
     if [ ! -d ${PXF_HOME} ]; then
         if [ -d pxf_tarball ]; then
-            # during release pipeline, install from tarball
             tar -xzf pxf_tarball/pxf.tar.gz -C ${GPHOME}
         else
-            # dev pipeline: build from source
             export BUILD_NUMBER="${TARGET_OS}"
             export JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF8
             pushd pxf_src/server
-            su - gpadmin -c "make -C '$(pwd)' install"
+            make install
             popd
         fi
         chown -R gpadmin:gpadmin ${PXF_HOME}
