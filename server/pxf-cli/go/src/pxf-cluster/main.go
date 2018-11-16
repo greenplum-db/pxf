f6738eff1c110c1654025e87a511005e0c76f373
Lav Jain <ljain@pivotal.io>
2018-11-15 16:29:08 -0800

    Output error messages and host id when something fails
    Co-authored-by: Oliver Albertini <oalbertini@pivotal.io>
    Co-authored-by: Lav Jain <ljain@pivotal.io>

--- server/pxf-cli/go/src/pxf-cluster/main.go
+++ server/pxf-cli/go/src/pxf-cluster/main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"fmt"
-	"github.com/greenplum-db/gp-common-go-libs/cluster"
-	"github.com/greenplum-db/gp-common-go-libs/dbconn"
-	"github.com/greenplum-db/gp-common-go-libs/gplog"
 	"log"
 	"os"
 	"pxf-cluster/pxf"
 	"strings"
+
+	"github.com/greenplum-db/gp-common-go-libs/cluster"
+	"github.com/greenplum-db/gp-common-go-libs/dbconn"
+	"github.com/greenplum-db/gp-common-go-libs/gplog"
 )
 
 func main() {
@@ -28,21 +29,34 @@ func main() {
 	segConfigs := cluster.MustGetSegmentConfiguration(connection)
 
 	globalCluster := cluster.NewCluster(segConfigs)
-	var hostNames []string
-	for _, config := range segConfigs {
-		hostNames = append(hostNames, config.Hostname)
-	}
 
 	hostList := cluster.ON_HOSTS
 	if inputs.Args[0] == "init" {
 		hostList = cluster.ON_HOSTS_AND_MASTER
 	}
-	globalCluster.GenerateAndExecuteCommand(
-		fmt.Sprintf("Executing %s on all hosts", inputs.Args[0]),
+	remoteOut := globalCluster.GenerateAndExecuteCommand(
+		fmt.Sprintf("Executing command '%s' on all hosts", inputs.Args[0]),
 		func(contentID int) string {
-		    return strings.Join(pxf.RemoteCommandToRunOnSegments(inputs), " ")
-	    },
+			return strings.Join(pxf.RemoteCommandToRunOnSegments(inputs), " ")
+		},
 		hostList)
+	response := ""
+	errCount := 0
+	for index, error := range remoteOut.Stderrs {
+		host := globalCluster.Segments[index].Hostname
+		if len(error) > 0 {
+			response += fmt.Sprintf("%s:\n%s\n", host, error)
+			errCount++
+			continue
+		}
+		fmt.Printf("Command '%s' completed successfully on %s.\n", inputs.Args[0], host)
+	}
+	if errCount == 0 {
+		os.Exit(0)
+	}
+	response = fmt.Sprintf("Command '%s' failed on %d node[s]:\n\n", inputs.Args[0], errCount) + response
+	fmt.Printf("%s", response)
+	os.Exit(1)
 }
 
 func fatalOnError(err error) {
