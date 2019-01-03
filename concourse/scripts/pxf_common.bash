e4c1fc6cf004da7866bf5cdf7e464a850cddbe8b
Alexander Denissov <adenissov@pivotal.io>
2019-01-03 13:55:11 -0800

    Added minio docker image, test job and setup script

--- concourse/scripts/pxf_common.bash
+++ concourse/scripts/pxf_common.bash
@@ -291,3 +291,41 @@ function start_pxf_server() {
 
 	popd > /dev/null
 }
+
+function setup_minio() {
+    local GPHD_ROOT=${1}
+
+    echo 'Setting up Minio ...'
+    local accessKey=$(jq -r .credential.accessKey /opt/minio/data/.minio.sys/config/config.json)
+    local secretKey=$(jq -r .credential.accessKey /opt/minio/data/.minio.sys/config/config.json)
+    echo "Minio accessKey=${accessKey} secretKey=${secretKey}"
+    echo 'Starting Minio'
+    /opt/minio/bin/minio server /opt/minio/data &
+    if [[ -f ${GPHD_ROOT}/hadoop/etc/hadoop/core-site.xml ]]; then
+        echo 'Adding Minio configuration to core-site.xml'
+        cat >/tmp/minio.xml <<EOF
+<property>
+    <name>fs.s3a.endpoint</name>
+    <description>AWS S3 endpoint to connect to.</description>
+    <value>http://localhost:9000</value>
+</property>
+<property>
+    <name>fs.s3a.access.key</name>
+    <description>AWS access key ID.</description>
+    <value>${accessKey}</value>
+</property>
+<property>
+    <name>fs.s3a.secret.key</name>
+    <description>AWS secret key.</description>
+    <value>${secretKey}</value>
+</property>
+<property>
+    <name>fs.s3a.path.style.access</name>
+    <value>true</value>
+    <description>Enable S3 path style access.</description>
+</property>
+EOF
+        sed -i -e '/<configuration>/r /tmp/minio.xml' ${GPHD_ROOT}/hadoop/etc/hadoop/core-site.xml
+        rm /tmp/minio.xml
+    fi
+}
