ac151d994e79cddf97ede45751e54840e51d1f2c
Alexander Denissov <adenissov@pivotal.io>
2019-01-07 17:29:56 -0800

    Use MINIO env variables for creds

--- concourse/scripts/pxf_common.bash
+++ concourse/scripts/pxf_common.bash
@@ -295,15 +295,15 @@ function start_pxf_server() {
 function setup_minio() {
     echo 'Adding test bucket gpdb-ud-scratch to Minio ...'
     mkdir -p /opt/minio/data/gpdb-ud-scratch
+
+    export MINIO_ACCESS_KEY=admin
+    export MINIO_SECRET_KEY=password
+    echo "Minio credentials: accessKey=${MINIO_ACCESS_KEY} secretKey=${MINIO_SECRET_KEY}"
+
     echo 'Starting Minio ...'
     /opt/minio/bin/minio server /opt/minio/data &
-    sleep 3
-
-    local accessKey=$(jq -r .credential.accessKey /opt/minio/data/.minio.sys/config/config.json)
-    local secretKey=$(jq -r .credential.secretKey /opt/minio/data/.minio.sys/config/config.json)
-    echo "Minio accessKey=${accessKey} secretKey=${secretKey}"
 
     # export minio credentials as AWS environment variables
-    export AWS_ACCESS_KEY_ID=${accessKey}
-    export AWS_SECRET_ACCESS_KEY=${secretKey}
+    export AWS_ACCESS_KEY_ID=${MINIO_ACCESS_KEY}
+    export AWS_SECRET_ACCESS_KEY=${MINIO_SECRET_KEY}
 }
