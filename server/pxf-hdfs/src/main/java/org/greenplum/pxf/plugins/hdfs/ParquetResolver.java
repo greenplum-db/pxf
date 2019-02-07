f79dd7eae42ae398eb19cd7b7b73a2820937d4f7
Alexander Denissov <adenissov@pivotal.io>
2019-02-06 17:50:41 -0800

    removed support for complex types that was not yet ready

--- server/pxf-hdfs/src/main/java/org/greenplum/pxf/plugins/hdfs/ParquetResolver.java
+++ server/pxf-hdfs/src/main/java/org/greenplum/pxf/plugins/hdfs/ParquetResolver.java
@@ -21,11 +21,9 @@ package org.greenplum.pxf.plugins.hdfs;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.apache.parquet.example.data.Group;
 import org.apache.parquet.example.data.simple.SimpleGroupFactory;
 import org.apache.parquet.io.api.Binary;
-import org.apache.parquet.schema.GroupType;
 import org.apache.parquet.schema.MessageType;
 import org.apache.parquet.schema.OriginalType;
 import org.apache.parquet.schema.Type;
@@ -34,14 +32,12 @@ import org.greenplum.pxf.api.OneRow;
 import org.greenplum.pxf.api.io.DataType;
 import org.greenplum.pxf.api.model.BasePlugin;
 import org.greenplum.pxf.api.model.Resolver;
-import org.greenplum.pxf.plugins.hdfs.utilities.HdfsUtilities;
 
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -50,10 +46,6 @@ import static org.apache.parquet.schema.Type.Repetition.REPEATED;
 public class ParquetResolver extends BasePlugin implements Resolver {
 
     private static final int SECOND_IN_MILLIS = 1000;
-    private static final String COLLECTION_DELIM = ",";
-    private static final String MAPKEY_DELIM = ":";
-    private String collectionDelim;
-    private String mapkeyDelim;
     private static final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
     private MessageType schema;
@@ -73,11 +65,6 @@ public class ParquetResolver extends BasePlugin implements Resolver {
                 output.add(resolvePrimitive(group, columnIndex, type, 0));
             } else {
                 throw new UnsupportedOperationException("Parquet complex type support is not yet available.");
-//                int repeatCount = group.getFieldRepetitionCount(fieldIndex);
-//                for (int repeatIndex = 0; repeatIndex < repeatCount; repeatIndex++) {
-//                    output.add(resolveComplex(group.getGroup(fieldIndex, repeatIndex), type.asGroupType()));
-//                }
-//                output.add(resolveComplex(group.getGroup(columnIndex, 0), type.asGroupType(), 0));
             }
         }
         return output;
@@ -161,72 +148,6 @@ public class ParquetResolver extends BasePlugin implements Resolver {
         }
     }
 
-    private OneField resolveComplex(Group g, GroupType groupType, int level) {
-
-        level++;
-        ObjectNode node = mapper.createObjectNode();
-
-        List<Type> types = groupType.getFields();
-        OneField result = new OneField();
-        result.type = DataType.TEXT.getOID();
-
-        List<OneField> fieldList = new ArrayList<>();
-        int fieldCount = types.size();
-
-        for (int fieldIndex = 0; fieldIndex < fieldCount; fieldIndex++) {
-            Type type = types.get(fieldIndex);
-            //OneField field = new OneField();
-            //field.type = DataType.TEXT.getOID();
-            int repeatCount = g.getFieldRepetitionCount(fieldIndex);
-
-            // primitive ? --> cal our method, get either value or json back, hook to key
-
-            Object value = resolvePrimitive(g, fieldIndex, type, level).val;
-            //node.put(type.getName(), )
-
-
-            for (int repeatIndex = 0; repeatIndex < repeatCount; repeatIndex++) {
-                if (type.isPrimitive()) {
-                    if (type.getOriginalType() != null) {
-                        // we have a primitive that is part of key, value pair
-                        // TODO: create JsonNode
-                        //field.val = type.getName() + mapkeyDelim + g.getValueToString(fieldIndex, repeatIndex);
-                    } else {
-                        // we have a primitive in an array
-                        //field.val = g.getValueToString(fieldIndex, repeatIndex);
-                    }
-                    //fieldList.add(field);
-                } else {
-                    fieldList.add(resolveComplex(g.getGroup(fieldIndex, repeatIndex), type.asGroupType(), level));
-                }
-            }
-        }
-
-        if (fieldCount == 1 && types.get(0).isPrimitive()) {
-            // Primitive type within List
-            result.val = fieldList.get(0).val;
-        } else if (OriginalType.LIST == groupType.getOriginalType()) {
-            // List type
-            result.val = String.format("[%s]", HdfsUtilities.toString(fieldList, collectionDelim));
-        }
-        else {
-            // Struct type
-            result.val = String.format("{%s}", HdfsUtilities.toString(fieldList, collectionDelim));
-        }
-
-        return result;
-    }
-
-//    private OneField resolveArray(int index, Group g, Type type) {
-//        List<OneField> fieldList = new LinkedList<>();
-//        for (int i = 0; i < g.getFieldRepetitionCount(index); i++)
-//            fieldList.add(resolvePrimitive(index, g, type));
-//        OneField field = new OneField();
-//        field.type = DataType.TEXT.getOID();
-//        field.val = String.format("[%s]", HdfsUtilities.toString(fieldList, collectionDelim));
-//        return field;
-//    }
-
     private OneField resolvePrimitive(Group group, int columnIndex, Type type, int level) {
 
         OneField field = new OneField();
@@ -258,6 +179,9 @@ public class ParquetResolver extends BasePlugin implements Resolver {
                 // just return the array node within OneField container
                 field.val = jsonArray;
             }
+        } else {
+            // level > 0 and type != REPEATED -- primitive type as a member of complex group -- NOT YET SUPPORTED
+            throw new UnsupportedOperationException("Parquet complex type support is not yet available.");
         }
         return field;
     }
