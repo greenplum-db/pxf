c9aac9695246c251a5f5de75f97c64550451b16b
Alexander Denissov <adenissov@pivotal.io>
2019-04-24 12:26:53 -0700

    Added integration test for JDBC session parameters

---
+++ automation/tincrepo/main/pxf/features/jdbc/session_params/sql/query01.sql
@@ -0,0 +1,5 @@
+-- @description query01 for JDBC query from view
+SELECT * FROM pxf_jdbc_read_view_no_params WHERE name='client_min_messages';
+
+SELECT * FROM pxf_jdbc_read_view_session_params WHERE name='client_min_messages';
+
