a690d23ba833d8fe27c9d3488d6d7eb5dbb775dd
Shivram Mani <smani@pivotal.io>
2019-05-30 15:28:46 -0700

    Make pxf threads configurable
    Authored-by: Shivram Mani <smani@pivotal.io>

--- server/pxf-service/src/scripts/pxf-env-default.sh
+++ server/pxf-service/src/scripts/pxf-env-default.sh
@@ -62,6 +62,9 @@ export PXF_SHUTDOWN_PORT=${PXF_SHUTDOWN_PORT:=5889}
 # Memory
 export PXF_JVM_OPTS=${PXF_JVM_OPTS:="-Xmx2g -Xms1g"}
 
+# Threads
+export PXF_THREADS=${PXF_THREADS:=300}
+
 # Kerberos path to keytab file owned by pxf service with permissions 0400
 export PXF_KEYTAB=${PXF_KEYTAB:="${PXF_CONF}/keytabs/pxf.service.keytab"}
 
