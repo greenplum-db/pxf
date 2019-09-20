340e3d220ed4ac4bd65c83b76714a71505765828
Alexander Denissov <adenissov@pivotal.io>
2019-09-20 12:21:48 -0700

    added properties to make HDP3 work

--- concourse/scripts/test_pxf.bash
+++ concourse/scripts/test_pxf.bash
@@ -82,6 +82,9 @@ function setup_hadoop() {
 	if [[ -n ${GROUP} ]]; then
 		export SLAVES=1
 		setup_impersonation ${hdfsrepo}
+		if [[ $(grep 'hadoop-3' ${hdfsrepo}/versions.txt) ]]; then
+		    adjust_for_hadoop3 ${hdfsrepo}
+		fi
 		start_hadoop_services ${hdfsrepo}
 	fi
 }
