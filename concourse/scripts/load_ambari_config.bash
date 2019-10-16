1bc6c6ce6d3da02cb450e1201e6e81988de42900
Francisco Guerrero <aguerrero@pivotal.io>
2019-10-16 15:21:04 -0700

    copy files inside folder

--- concourse/scripts/load_ambari_config.bash
+++ concourse/scripts/load_ambari_config.bash
@@ -7,4 +7,4 @@ PROJECT=${GOOGLE_PROJECT_ID:-}
 gcloud config set project "$PROJECT"
 gcloud auth activate-service-account --key-file=<(echo "$GOOGLE_CREDENTIALS")
 
-gsutil cp -r gs://data-gpdb-ud/configuration/ambari-cloud/ ambari_env_files
+gsutil cp -r gs://data-gpdb-ud/configuration/ambari-cloud/* ambari_env_files
