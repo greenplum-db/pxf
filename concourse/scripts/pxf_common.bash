ff6e542b7a95717c81ae65e18ea032eebc205e8a
Alexander Denissov <adenissov@pivotal.io>
2020-04-22 21:22:30 -0700

    update MASTER_DATA_DIRECTORY

--- concourse/scripts/pxf_common.bash
+++ concourse/scripts/pxf_common.bash
@@ -173,8 +173,7 @@ function add_remote_user_access_for_gpdb() {
 		if [[ -f gpdb_src/gpAux/gpdemo/gpdemo-env.sh ]]; then
 		    source gpdb_src/gpAux/gpdemo/gpdemo-env.sh
 		else
-		    export MASTER_DATA_DIRECTORY=~gpadmin/data/master
-		    echo 'Setting MASTER_DATA_DIRECTORY=\${MASTER_DATA_DIRECTORY}'
+		    export MASTER_DATA_DIRECTORY=~gpadmin/data/master/gpseg-1
 		fi
 		echo 'local    all     ${username}     trust' >> \${MASTER_DATA_DIRECTORY}/pg_hba.conf
 		source ${GPHOME}/greenplum_path.sh
