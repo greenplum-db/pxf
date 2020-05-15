1023c831e466de6a719ddbcb6eec0093ee85c0a1
Francisco Guerrero <aguerrero@pivotal.io>
2020-05-15 16:46:37 -0700

    Fix expected error message on cloud

--- automation/tincrepo/main/pxf/features/hcfs/globbing/match_string_from_string_set/sql/query04.sql
+++ automation/tincrepo/main/pxf/features/hcfs/globbing/match_string_from_string_set/sql/query04.sql
@@ -39,14 +39,17 @@ select * from hcfs_glob_match_string_from_string_set_10 order by name, num;
 -- m/(ERROR|WARNING):.*remote component error.*\(\d+\).*from.*'(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])):\d+'.*/
 -- s/'(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])):\d+'/'SOME_IP:SOME_PORT'/
 --
--- m/Exception Report.*(java.io.IOException: Illegal file pattern: Unclosed group near index 4).*/
--- s/Report.*/Illegal file pattern: Unclosed group near index 4/
+-- m/Exception Report.*(java.io.IOException: Illegal file pattern: Unclosed group near index).*/
+-- s/Report.*/Illegal file pattern: Unclosed group near index xxx/
+--
+-- m/Exception Report.*(java.util.regex.PatternSyntaxException: Unclosed group near index).*/
+-- s/Report.*/Illegal file pattern: Unclosed group near index xxx/
 --
 -- m/DETAIL/
 -- s/DETAIL/CONTEXT/
 --
 -- m/pxf:\/\/(.*)\/pxf_automation_data/
--- s/pxf:\/\/.*match_string_from_string_set_4/pxf:\/\/pxf_automation_data?PROFILE/
+-- s/pxf:\/\/.*match_string_from_string_set_4.*/pxf:\/\/pxf_automation_data?PROFILE=*:text/
 --
 -- m/CONTEXT:.*line.*/
 -- s/line \d* of //g
