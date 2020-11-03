0cf0fd3479bdb9112f7bcaaef787c1b8f535a191
Alexander Denissov <adenissov@pivotal.io>
2020-11-02 21:15:12 -0800

    added unit test for pruner

--- server/pxf-hive/src/main/java/org/greenplum/pxf/plugins/hive/HiveAccessor.java
+++ server/pxf-hive/src/main/java/org/greenplum/pxf/plugins/hive/HiveAccessor.java
@@ -27,7 +27,6 @@ import org.apache.commons.lang.StringUtils;
 import org.apache.hadoop.hive.common.type.HiveDecimal;
 import org.apache.hadoop.hive.conf.HiveConf;
 import org.apache.hadoop.hive.ql.io.IOConstants;
-import org.apache.hadoop.hive.ql.io.orc.Reader;
 import org.apache.hadoop.hive.ql.io.sarg.ConvertAstToSearchArg;
 import org.apache.hadoop.hive.ql.io.sarg.SearchArgument;
 import org.apache.hadoop.hive.serde.serdeConstants;
@@ -635,7 +634,7 @@ public class HiveAccessor extends HdfsSplittableDataAccessor {
         Node root = new FilterParser().parse(filterStr);
 
         // Prune the parsed tree with valid supported datatypes and operators and then
-        // traverse the pruned tree with the searchArgumentBuilder to produce a SearchArgument for ORC
+        // traverse the pruned tree with the searchArgumentBuilder to produce a SearchArgument
         TRAVERSER.traverse(
                 root,
                 new SupportedDataTypePruner(context.getTupleDescription(), getSupportedDatatypesForPushdown()),
