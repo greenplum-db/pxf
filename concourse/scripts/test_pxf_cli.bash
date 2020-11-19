2dd6a85d725c1cdab77c2ece8d0a28e51ab42423
Alexander Denissov <adenissov@pivotal.io>
2020-11-19 12:03:05 -0800

    added standby master to multinode-no-imper

--- concourse/scripts/test_pxf_cli.bash
+++ concourse/scripts/test_pxf_cli.bash
@@ -8,15 +8,11 @@ cp -r "${cluster_env_files}/.ssh" "${HOME}"
 
 scp "${SCRIPT_DIR}/cli/common.sh" mdw:
 
-if [[ -n "${PXF_BASE_DIR}" ]]; then
-  PXF_BASE_DIR_OPT="PXF_BASE_DIR=${PXF_BASE_DIR} "
-fi
-
 err_cnt=0
 for script in "${SCRIPT_DIR}/cli/"test_*.sh; do
 	scp "${script}" mdw:
 	script_short_name=${script##*/} # chop off path to script
-	ssh mdw "${PXF_BASE_DIR_OPT}~gpadmin/${script_short_name}"
+	ssh mdw "~gpadmin/${script_short_name}"
 	((err_cnt+=$?))
 done
 
