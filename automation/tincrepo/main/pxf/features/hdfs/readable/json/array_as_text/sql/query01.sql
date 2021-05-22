939a73d4ce9839166b3dd12b68881dd898b0b288
Alexander Denissov <adenissov@pivotal.io>
2021-05-21 18:05:27 -0700

    added projection query with JSON functions

--- automation/tincrepo/main/pxf/features/hdfs/readable/json/array_as_text/sql/query01.sql
+++ automation/tincrepo/main/pxf/features/hdfs/readable/json/array_as_text/sql/query01.sql
@@ -1,3 +1,6 @@
 -- @description query01 for PXF HDFS Readable Json arrays as text test cases
 
-SELECT * from jsontest_array_as_text ORDER BY id;
+SELECT * FROM jsontest_array_as_text ORDER BY id;
+
+SELECT id, (num_arr::json)->0 "num", (bool_arr::json)->1 "bool", (str_arr::json)->2 "str", (arr_arr::json)->0 "arr",
+(obj_arr::json)->1 "obj", (obj::json)->'data'->'data'->'key' "key" FROM jsontest_array_as_text ORDER BY id;
