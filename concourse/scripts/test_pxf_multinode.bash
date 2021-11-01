0a9614eb9c55887874b8f6ff0413bdd972e844b0
Alexander Denissov <adenissov@pivotal.io>
2021-11-01 11:37:53 -0700

    added creation of hdfs-ipa-no-impersonation PXF server

--- concourse/scripts/test_pxf_multinode.bash
+++ concourse/scripts/test_pxf_multinode.bash
@@ -313,8 +313,21 @@ function setup_pxf_kerberos_on_cluster() {
 		"
 
 		scp ipa_env_files/conf/*-site.xml "gpadmin@mdw:${BASE_DIR}/servers/hdfs-ipa"
-		ssh gpadmin@mdw "PXF_BASE=${BASE_DIR} ${PXF_HOME}/bin/pxf cluster sync"
 
+		# optionally create a non-impersonation configuration server for the IPA cluster for the proxy test
+		# need to set constrained delegation to false if impersonation is turned off
+		ssh gpadmin@mdw "
+			if [[ ${IMPERSONATION} == true ]]; then
+				cp -r ${BASE_DIR}/servers/hdfs-ipa ${BASE_DIR}/servers/hdfs-ipa-no-impersonation
+				sed -i \
+				-e '/<name>pxf.service.user.impersonation<\/name>/ {n;s|<value>.*</value>|<value>false</value>|g;}' \
+				-e 's|</configuration>|<property><name>pxf.service.user.name</name><value>foobar</value></property></configuration>|g' \
+				-e '/pxf.service.kerberos.constrained-delegation/{n;s|<value>.*</value>|<value>false</value>|}' \
+				${BASE_DIR}/servers/hdfs-ipa-no-impersonation/pxf-site.xml
+			fi
+		"
+
+		ssh gpadmin@mdw "PXF_BASE=${BASE_DIR} ${PXF_HOME}/bin/pxf cluster sync"
 		sed -i \
 			-e "s|</hdfsIpa>|<hadoopRoot>${HADOOP_3_DIR}</hadoopRoot><testKerberosPrincipal>${HADOOP_3_USER}@${REALM3}</testKerberosPrincipal></hdfsIpa>|g" \
 			"$multiNodesCluster"
