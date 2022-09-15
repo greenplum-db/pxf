4427c3af21f4bbe19506bdb5709a4728ca4a04ef
Yiming Li <yimingli@vmware.com>
2022-09-14 17:46:46 -0700

    finish date_array
    Authored-by: Yiming Li <yimingli@vmware.com>

--- server/pxf-hdfs/src/main/java/org/greenplum/pxf/plugins/hdfs/ParquetResolver.java
+++ server/pxf-hdfs/src/main/java/org/greenplum/pxf/plugins/hdfs/ParquetResolver.java
@@ -256,12 +256,10 @@ public class ParquetResolver extends BasePlugin implements Resolver {
             if (vals.get(i) != null) {
                 switch (elementType.asPrimitiveType().getPrimitiveTypeName()) {
                     case INT32:
-//                        if (type.getLogicalTypeAnnotation() instanceof DateLogicalTypeAnnotation) {
-//                            String dateString = (String) field.val;
-//                            group.add(index, ParquetTypeConverter.getDaysFromEpochFromDateString(dateString));
-//                        }
-                        if( elementType.getLogicalTypeAnnotation() instanceof  IntLogicalTypeAnnotation &&
-                                 ((IntLogicalTypeAnnotation) elementType.getLogicalTypeAnnotation()).getBitWidth() ==16){
+                        if (elementType.getLogicalTypeAnnotation() instanceof DateLogicalTypeAnnotation) {
+                            repeatedGroup.add(index, (Integer) vals.get(i));
+                        } else if (elementType.getLogicalTypeAnnotation() instanceof IntLogicalTypeAnnotation &&
+                                ((IntLogicalTypeAnnotation) elementType.getLogicalTypeAnnotation()).getBitWidth() == 16) {
                             repeatedGroup.add(0, (Short) vals.get(i));
                         } else {
                             repeatedGroup.add(0, (Integer) vals.get(i));
