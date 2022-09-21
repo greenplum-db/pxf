0ac3f490265bb63d7f697dfeb586b4718e7b7801
Yiming Li <yimingli@vmware.com>
2022-09-20 17:14:22 -0700

    Rename parquet write unit test
    Authored-by: Yiming Li <yimingli@vmware.com>

--- server/pxf-hdfs/src/test/java/org/greenplum/pxf/plugins/hdfs/ParquetResolverWriteTest.java
+++ server/pxf-hdfs/src/test/java/org/greenplum/pxf/plugins/hdfs/ParquetResolverWriteTest.java
@@ -61,7 +61,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class ParquetWriteTest {
+public class ParquetResolverWriteTest {
 
     private Accessor accessor;
     private Resolver resolver;
