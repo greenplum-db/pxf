8e9eacbe58137be2874278aed214adbef7e82bec
Yiming Li <yimingli@vmware.com>
2022-09-27 18:33:15 -0700

    WIP: integration test
    Authored-by: Yiming Li <yimingli@vmware.com>

---
+++ automation/tincrepo/main/pxf/features/parquet/write/primitive_array_types_with_hive/sql/query01.sql
@@ -0,0 +1,7 @@
+-- @description query01 for writing parquet primitive array data types using the Hive profile
+SET bytea_output=hex;
+
+-- for the numeric column in GPDB, decimal(38,18) in Hive, there are differences in output between different hive versions.
+-- make the print consistent by doing `round(numeric_arr, 18) as numeric_arr` to force the same precision.
+SELECT id, bool_arr, bytea_arr, bigint_arr, small_arr, int_arr, text_arr, real_arr, float_arr, char_arr, varchar_arr, varchar_arr_nolimit, date_arr, timestamp_arr, round(numeric_arr, 18) as numeric, FROM pxf_parquet_write_array_with_hive_readable ORDER BY id;
+
