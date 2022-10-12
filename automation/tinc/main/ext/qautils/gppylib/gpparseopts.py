27a0fa4f43a6b97924711d11f575ebabd846d950
Himanshu Pandey <pandeyhi@vmware.com>
2022-10-11 17:56:57 -0700

    Add support for GP7 on RHEL 8

--- automation/tinc/main/ext/qautils/gppylib/gpparseopts.py
+++ automation/tinc/main/ext/qautils/gppylib/gpparseopts.py
@@ -73,13 +73,13 @@ class OptChecker(Option):
     def datetimeCheck(option, opt, value):
         try:
             return str_to_datetime(value)
-        except DatetimeValueError, e:
+        except DatetimeValueError as e:
             raise OptionValueError('"%s %s" ... %s' % (opt, value, e.description))
             
     def durationCheck(option, opt, value):
         try:
             return str_to_duration(value, signed=False)
-        except DatetimeValueError, e:
+        except DatetimeValueError as e:
             raise OptionValueError('"%s %s" ... %s' % (opt, value, e.description))
             
     def regexCheck(option, opt, value):
@@ -90,7 +90,7 @@ class OptChecker(Option):
             flags |= re.IGNORECASE
         try:
             return re.compile(value, flags)
-        except Exception, e:
+        except Exception as e:
             raise OptionValueError('"%s %s" ... %s' % (opt, value, e))
  
     def regexSetCaseSensitivity(option, opt, value, parser):
