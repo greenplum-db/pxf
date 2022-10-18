97378cb726e7b23f698f4706f2b70cdf5b439245
Yiming Li <yimingli@vmware.com>
2022-10-18 13:02:36 -0700

    Upload parquet array dataset without null and timestamp
    Authored-by: Yiming Li <yimingli@vmware.com>

---
+++ automation/src/test/resources/data/parquet/generate_parquet_list_types_without_null.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+# Run this command to generate the parquet_list_types.parquet file
+SRC_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+HDFS_CMD=${HDFS_CMD:-~/workspace/singlecluster-HDP3/bin/hdfs}
+HIVE_CMD=${HIVE_CMD:-~/workspace/singlecluster-HDP3/bin/hive}
+
+$HIVE_CMD -f "$SRC_DIR"/generate_parquet_list_types_without_null.hql
+
+# Copy file to the directory where this script resides
+rm -f "$SRC_DIR"/parquet_list_types_without_null.parquet
+$HDFS_CMD dfs -copyToLocal /hive/warehouse/parquet_list_types_without_null/000000_0 "$SRC_DIR"/parquet_list_types_without_null.parquet
+rm -f "/Users/yimingli/workspace/pxf/server/pxf-hdfs/src/test/resources/parquet/parquet_list_types_without_null.parquet"
+$HDFS_CMD dfs -copyToLocal /hive/warehouse/parquet_list_types_without_null/000000_0 "/Users/yimingli/workspace/pxf/server/pxf-hdfs/src/test/resources/parquet/parquet_list_types_without_null.parquet"
