2382042ecec0eb46f0ae07c9e4a3e3c66a867bd6
Yiming Li <yimingli@vmware.com>
2022-11-18 15:24:33 -0800

    Update prepareReadableExternalTable
    Authored-by: Yiming Li <yimingli@vmware.com>

--- automation/src/test/java/org/greenplum/pxf/automation/features/parquet/ParquetReadTest.java
+++ automation/src/test/java/org/greenplum/pxf/automation/features/parquet/ParquetReadTest.java
@@ -4,6 +4,7 @@ import org.greenplum.pxf.automation.features.BaseFeature;
 import org.greenplum.pxf.automation.structures.tables.basic.Table;
 import org.greenplum.pxf.automation.structures.tables.utils.TableFactory;
 import org.greenplum.pxf.automation.structures.tables.pxf.ReadableExternalTable;
+import org.greenplum.pxf.automation.structures.tables.utils.TableFactory;
 import org.greenplum.pxf.automation.utils.system.ProtocolEnum;
 import org.greenplum.pxf.automation.utils.system.ProtocolUtils;
 import org.testng.annotations.Test;
