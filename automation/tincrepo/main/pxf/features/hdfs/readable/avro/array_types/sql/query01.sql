5773216c7b6b60d2ed37f6017be6b402843c088d
Himanshu Pandey <pandeyhi@vmware.com>
2023-02-14 13:23:53 -0800

    Fixing avro tests for GP versions <7

--- automation/tincrepo/main/pxf/features/hdfs/readable/avro/array_types/sql/query01.sql
+++ automation/tincrepo/main/pxf/features/hdfs/readable/avro/array_types/sql/query01.sql
@@ -1,7 +1,7 @@
 -- @description query01 for PXF HDFS Readable Avro supported array types test cases
 
-SELECT * from avrotest_arrays ORDER BY type_int, type_bytes desc, type_bytes_array desc;
-SELECT * from avrotest_arrays_gpdb_arrays ORDER BY type_int, type_bytes desc, type_bytes_array desc;
+SELECT * from avrotest_arrays ORDER BY type_int;
+SELECT * from avrotest_arrays_gpdb_arrays ORDER BY type_int;
 
-SELECT type_int_array, type_double_array, type_string_array, type_float_array, type_long_array, type_bytes_array, type_boolean_array from avrotest_arrays ORDER BY type_int, type_bytes desc, type_bytes_array desc;
-SELECT type_int_array, type_double_array, type_string_array, type_float_array, type_long_array, type_bytes_array, type_boolean_array from avrotest_arrays_gpdb_arrays ORDER BY type_int, type_bytes desc, type_bytes_array desc;
+SELECT type_int_array, type_double_array, type_string_array, type_float_array, type_long_array, type_bytes_array, type_boolean_array from avrotest_arrays ORDER BY type_int;
+SELECT type_int_array, type_double_array, type_string_array, type_float_array, type_long_array, type_bytes_array, type_boolean_array from avrotest_arrays_gpdb_arrays ORDER BY type_int;
