11a1e5a26ab6fe6d782e340c41885876273abd6b
Yiming Li <yimingli@vmware.com>
2023-03-01 14:39:41 -0800

    added automation tests for large precision numeric data

--- automation/tincrepo/main/pxf/features/parquet/decimal/numeric/sql/query01.sql
+++ automation/tincrepo/main/pxf/features/parquet/decimal/numeric/sql/query01.sql
@@ -1,2 +1,2 @@
--- @description query01 for writing defined precision numeric with NULL flag on. When try to write a numeric with precision > HiveDecimal.MAX_PRECISION, the numeric will be set to NULL
-SELECT a, b, c, d, e, f, g, h FROM pxf_parquet_read_numeric_with_null ORDER BY description;
+-- @description query01 for writing defined precision numeric with pxf.parquet.write.decimal.overflow = ignore. When try to write a numeric with precision > HiveDecimal.MAX_PRECISION, the numeric will be set to NULL
+SELECT a, b, c, d, e, f, g, h FROM pxf_parquet_read_numeric ORDER BY description;
