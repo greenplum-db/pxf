ce191eaf0bffe7989684fc376b70acca40e056a0
Yiming Li <yimingli@vmware.com>
2023-03-02 15:45:32 -0800

    format fix

--- automation/tincrepo/main/pxf/features/parquet/decimal/numeric/sql/query01.sql
+++ automation/tincrepo/main/pxf/features/parquet/decimal/numeric/sql/query01.sql
@@ -1,2 +1,2 @@
--- @description query01 for writing defined precision numeric with pxf.parquet.write.decimal.overflow = ignore. When try to write a numeric with precision > HiveDecimal.MAX_PRECISION, the numeric will be set to NULL
+-- @description query01 for writing defined precision numeric with data precision <= HiveDecimal.MAX_PRECISION
 SELECT a, b, c, d, e, f, g, h FROM pxf_parquet_read_numeric ORDER BY description;
