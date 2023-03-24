b61bcdad4bf1e327167a4eaff01b83279e0722c5
Ashuka Xue <axue@vmware.com>
2023-03-24 11:51:01 -0700

    remove unused import

--- automation/src/main/java/org/greenplum/pxf/automation/utils/csv/CsvUtils.java
+++ automation/src/main/java/org/greenplum/pxf/automation/utils/csv/CsvUtils.java
@@ -2,7 +2,6 @@ package org.greenplum.pxf.automation.utils.csv;
 
 import java.io.FileOutputStream;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.nio.charset.Charset;
