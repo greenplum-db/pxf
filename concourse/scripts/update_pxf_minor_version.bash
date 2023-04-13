a1878e0636de498dea70cf5495c9206b32497717
Ashuka Xue <axue@vmware.com>
2023-04-13 10:41:58 -0700

    fixup! fixup permissions, add print

--- concourse/scripts/update_pxf_minor_version.bash
+++ concourse/scripts/update_pxf_minor_version.bash
@@ -14,7 +14,7 @@ echoRed() { echo $'\e[0;31m'"$1"$'\e[0m'; }
 echoGreen() { echo $'\e[0;32m'"$1"$'\e[0m'; }
 
 function upgrade_pxf() {
-  existing_pxf_version=$(cat $PXF_HOME/version)
+	existing_pxf_version=$(cat $PXF_HOME/version)
 	echoGreen "Stopping PXF ${existing_pxf_version}"
 	${PXF_HOME}/bin/pxf version && ${PXF_HOME}/bin/pxf cluster stop
 
@@ -32,18 +32,18 @@ function upgrade_pxf() {
 		PXF_BASE=${PXF_BASE_DIR} ${PXF_HOME}/bin/pxf cluster prepare
 		echo \"export PXF_BASE=${PXF_BASE_DIR}\" >> ~gpadmin/.bashrcz
 	fi
-  updated_pxf_version=$(cat $PXF_HOME/version)
+	updated_pxf_version=$(cat $PXF_HOME/version)
 
 	echoGreen "Starting PXF ${updated_pxf_version}"
 
-	if [[ ${existing_pxf_version} -ge ${updated_pxf_version} ]]; then
-	  echoRed "Existing version of PXF (${existing_pxf_version}) is greater than or equal to the new version (${updated_pxf_version})"
+	if [[ ${existing_pxf_version} > ${updated_pxf_version} ]]; then
+		echoRed "Existing version of PXF (${existing_pxf_version}) is greater than or equal to the new version (${updated_pxf_version})"
 	fi
 
-	PXF_BASE=${PXF_BASE_DIR} ${PXF_HOME}/bin/pxf cluster start
+	su gpadmin -c "PXF_BASE=${PXF_BASE_DIR} ${PXF_HOME}/bin/pxf cluster start"
 
-  echoGreen "ALTER EXTENSION pxf UPDATE - for multibyte delimiter tests"
-  source ${GPHOME}/greenplum_path.sh && psql -d template1 -c 'ALTER EXTENSION pxf UPDATE'
-  source ${GPHOME}/greenplum_path.sh && psql -d pxfautomation -c 'ALTER EXTENSION pxf UPDATE'
-  source ${GPHOME}/greenplum_path.sh && psql -d pxfautomation_encoding -c 'SELECT * FROM pg_extension'
+	echoGreen "ALTER EXTENSION pxf UPDATE - for multibyte delimiter tests"
+	su gpadmin -c "source ${GPHOME}/greenplum_path.sh && psql -d template1 -c 'ALTER EXTENSION pxf UPDATE' \
+																							&& psql -d pxfautomation -c 'ALTER EXTENSION pxf UPDATE' \
+																							&& psql -d pxfautomation_encoding -c 'SELECT * FROM pg_extension'"
 }
