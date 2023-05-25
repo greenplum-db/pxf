4c6222c53ad82c994568d365581b8428599342c9
Yiming Li <yimingli@vmware.com>
2023-05-25 11:05:00 -0700

    fixup! push up the missing DecimalUtilities.java

--- server/pxf-hdfs/src/main/java/org/greenplum/pxf/plugins/hdfs/orc/ORCVectorizedResolver.java
+++ server/pxf-hdfs/src/main/java/org/greenplum/pxf/plugins/hdfs/orc/ORCVectorizedResolver.java
@@ -103,6 +103,8 @@ import static org.greenplum.pxf.plugins.hdfs.orc.ORCVectorizedAccessor.MAP_BY_PO
 public class ORCVectorizedResolver extends BasePlugin implements ReadVectorizedResolver, WriteVectorizedResolver, Resolver {
 
     private static final String UNSUPPORTED_ERR_MESSAGE = "Current operation is not supported";
+
+    private static final String PXF_ORC_WRITE_DECIMAL_OVERFLOW_PROPERTY_NAME = "pxf.orc.write.decimal.overflow";
     /**
      * The schema used to read or write the ORC file.
      */
@@ -144,6 +146,7 @@ public class ORCVectorizedResolver extends BasePlugin implements ReadVectorizedR
     private List<List<OneField>> cachedBatch;
     private VectorizedRowBatch vectorizedRowBatch;
 
+    private DecimalUtilities decimalUtilities = new DecimalUtilities();
     /**
      * {@inheritDoc}
      */
