dbfa42c7342bf6138f74e26d7f6e26cccda74dff
Yiming Li <yimingli@vmware.com>
2023-05-26 16:24:03 -0700

    update comments

--- server/pxf-hdfs/src/main/java/org/greenplum/pxf/plugins/hdfs/utilities/DecimalUtilities.java
+++ server/pxf-hdfs/src/main/java/org/greenplum/pxf/plugins/hdfs/utilities/DecimalUtilities.java
@@ -76,7 +76,7 @@ public class DecimalUtilities {
          (https://github.com/apache/orc/blob/de5831ffbc72e872f61b0afd9683b77bd09c6c61/java/core/src/java/org/apache/orc/TypeDescription.java#L39)
          so we parse the value into ORC format with HiveDecimal.
 
-         For Parquet, there is no decimal precision limitation, and the decimal precision and scale is pretty large in postgres,
+         For Parquet, there is no decimal precision limitation, and the maximum supported decimal precision and scale are pretty large in postgres,
          (https://www.postgresql.org/docs/9.3/datatype-numeric.html#DATATYPE-NUMERIC-TABLE)
          so we parse the value into Parquet with BigDecimal instead of HiveDecimal when precision is greater than Hive's max precision 38
          */
@@ -103,28 +103,30 @@ public class DecimalUtilities {
      */
     private BigDecimal parseDecimalStringWithHiveDecimal(String value, int precision, int scale, boolean enforcePrecisionAndScale, String columnName) {
         /*
-         HiveDecimal.create has different behaviors for different types of overflow:
+         Greenplum will handle the overflow which integer digit count (# digits to the left side of the decimal point) of
+         the incoming value is greater than the precision defined in the column e.g., NUMERIC(precision).
+         Therefore, the following part is to deal with the integer digit count overflow when no precision is defined in
+         the column e.g., NUMERIC.
+         By default, NUMERIC will be stored as DECIMAL(38,18) in Parquet's schema, or DECIMAL(38,10) in ORC's schema.
 
-         (1) Precision overflow
-         When the data integer digit count (data precision - data scale) is greater than 38,
-         HiveDecimal.create will return a null value. To make the behavior consistent with Hive's behavior
-         when storing on a Parquet-backed table, we store the value as null.
+         HiveDecimal.create has different behaviors depends on whether there is an integer digit count overflow and the
+         total digits of the incoming value.
+
+         (1) integer digit count > precision defined in schema
+         HiveDecimal.create will return a NULL value. To make the behavior consistent with Hive's behavior
+         when storing on a Parquet/ORC-backed table, we store the value as null.
          For example, the integer digit count of 1234567890123456789012345678901234567890.123 is 40,
          which is greater than 38. HiveDecimal.create returns null.
 
-         (2) Integer digit count overflow
-         When data integer digit count is not greater than 38,
-         and the overall data precision is greater than 38,
+         (2) (Integer digit count <= precision defined in schema) && (total digits of the value > precision defined in schema)
          HiveDecimal.create will return a rounded-off value to fit in the Hive maximum supported precision 38.
          For example, the integer digit count of 1234567890123456789012345.12345678901234567890 is 25 which is less than 38,
          but its overall precision is 45 which is greater than 38.
          Then data will be created as a rounded value 1234567890123456789012345.1234567890123
 
-         (3) Scale overflow
-         When data integer digit count is not greater than 38,
-         and the overall data precision is not greater than 38,
-         HiveDecimal.create will return the same decimal value as provided.
-         For example, 123456.123456 can fit in DECIMAL(38,18) without any data loss, so the data will be created as the same
+         (3) (Integer digit count <= precision defined in schema) && (total digits of the value <= precision defined in schema)
+         HiveDecimal.create will return exactly the same decimal value as provided.
+         For example, 123456.123456 can fit in DECIMAL(38,18) without any data loss.
          */
 
         // HiveDecimal.create will return a decimal value which can fit in DECIMAL(38)
@@ -148,11 +150,28 @@ public class DecimalUtilities {
             return null;
         }
 
-        // When enforcePrecisionAndScale = true, it will have a check on integer digit count (HiveDecimal.enforcePrecisionScale),
-        // and values will be rounded with the integer digit count not greater than the max integer digit count
-        // When enforcePrecisionAndScale = false, no check on integer digit count,
-        // and values will be rounded with the integer digit count not greater than the max precision
-        // so there will be different error hints in the scale check
+        /*
+        At this point, the integer digit count must less than precision, but the total digits may still greater than precision.
+        HiveDecimal.enforcePrecisionScale has different behaviors depends on the integer digit count and the max integer digit count (precision - scale).
+
+        (1) integer digit count > precision - scale
+        HiveDecimal.enforcePrecisionScale returns NULL. For example, the column will be stored as DECIMAL(38,18).
+        The integer digit count of 1234567890123456789012345.12345678901234567890 is 25 which is less than 38, but it's greater
+        than the max integer digit count 20. A null value is returned.
+
+        (2) (integer digit count <= precision - scale) && (total digits of the value > precision defined in schema)
+        HiveDecimal.enforcePrecisionScale will return a rounded-off value to fit in the precision and scale.
+        For example, the column will be stored as DECIMAL(38,18). The integer digit count of 1234567890.123456789012345678901234567890
+        is 10 which is less than 20, but total digits is 40 which is greater than 38. The value will be stored as a rounded one
+        1234567890.1234567890123456789012345679
+
+        (3) (integer digit count <= precision - scale) && (total digits of the value <= precision defined in schema)
+        HiveDecimal.enforcePrecisionScale will return exactly the same decimal value as provided.
+        For example, 123456.123456 can fit in DECIMAL(38,18) without any data loss.
+
+        Current logic for ORC doesn't call HiveDecimal.enforcePrecisionScale but Parquet does. That will bring in the
+        inconsistency in the error messages in the third check.
+         */
         String limitationForAccurateValue = String.format("maximum precision %s", precision);
         if (enforcePrecisionAndScale) {
             // At this point data can fit in precision 38, but still need enforcePrecisionScale to check whether it can fit in scale 18
@@ -161,16 +180,6 @@ public class DecimalUtilities {
                     precision,
                     scale);
 
-            /*
-            When data integer digit count is greater than the maximum supported integer digit count 20 (38 - 18),
-            enforcePrecisionScale will return null, it means we cannot store the value in Parquet because we have
-            exceeded the maximum integer digit count. To make the behavior consistent with Hive's behavior
-            when storing on a Parquet-backed table, we store the value as null.
-            For example, in the case 2 in the previous HiveDecimal.create example,
-            we got a rounded value 1234567890123456789012345.1234567890123.
-            Its integer digit count is 25 which exceeds the maximum integer digit count 20.
-            So it cannot fit in DECIMAL(38,18) and Null is returned.
-             */
             if (hiveDecimal == null) {
                 if (isDecimalOverflowOptionError || isDecimalOverflowOptionRound) {
                     throw new UnsupportedTypeException(String.format("The value %s for the NUMERIC column %s using %s profile exceeds maximum precision and scale (%d,%d).",
@@ -192,8 +201,9 @@ public class DecimalUtilities {
             limitationForAccurateValue = String.format("maximum scale %s", scale);
         }
 
+        // At this point, the integer digit count must less than (precision - scale), but the total digits may still greater than precision.
+        // Here is to check whether there is a precision loss.
         BigDecimal accurateDecimal = new BigDecimal(value);
-        // At this point data can fit in DECIMAL(38,18), but may have been rounded off
         if ((isDecimalOverflowOptionError || isDecimalOverflowOptionRound) && accurateDecimal.compareTo(hiveDecimal.bigDecimalValue()) != 0) {
             if (isDecimalOverflowOptionError) {
                 throw new UnsupportedTypeException(String.format("The value %s for the NUMERIC column %s using %s profile exceeds %s, and cannot be stored without precision loss.",
