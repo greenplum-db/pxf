1a92346ace7d77970c949dba89758a236ac6dd53
Alexander Denissov <adenissov@vmware.com>
2023-06-14 19:21:31 -0700

    another fix for tmz type

--- automation/tincrepo/main/pxf/features/hdfs/writable/json/primitive_types_rows/sql/query01.sql
+++ automation/tincrepo/main/pxf/features/hdfs/writable/json/primitive_types_rows/sql/query01.sql
@@ -9,6 +9,6 @@
 \pset null 'NIL'
 SET bytea_output = 'hex';
 
-SELECT * from gpdb_primitive_types ORDER BY id;
+SELECT id, name, sml, integ, bg, r, dp, dec, bool, cdate, ctime, tm, CAST(tmz AS TIMESTAMP WITH TIME ZONE) AT TIME ZONE 'PDT' as tmz, c1, vc1, bin from gpdb_primitive_types ORDER BY id;
 
 SELECT id, name, sml, integ, bg, r, dp, dec, bool, cdate, ctime, tm, CAST(tmz AS TIMESTAMP WITH TIME ZONE) AT TIME ZONE 'PDT' as tmz, c1, vc1, bin from pxf_primitive_types_rows_json_read ORDER BY id;
