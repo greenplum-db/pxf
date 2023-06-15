086fe79bc976b2696981551d8ad19b399b22481d
Alexander Denissov <adenissov@vmware.com>
2023-06-14 17:49:02 -0700

    display timestamp with tz in PDT

--- automation/tincrepo/main/pxf/features/hdfs/writable/json/primitive_types_object/sql/query01.sql
+++ automation/tincrepo/main/pxf/features/hdfs/writable/json/primitive_types_object/sql/query01.sql
@@ -11,4 +11,4 @@ SET bytea_output = 'hex';
 
 SELECT * from gpdb_primitive_types ORDER BY id;
 
-SELECT * from pxf_primitive_types_object_json_read ORDER BY id;
+SELECT id, name, sml, integ, bg, r, dp, dec, bool, cdate, ctime, tm, CAST(tmz AS TIMESTAMP WITH TIME ZONE) AT TIME ZONE 'PDT' as tmz, c1, vc1, bin from pxf_primitive_types_object_json_read ORDER BY id;
