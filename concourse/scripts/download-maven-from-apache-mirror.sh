457be362ac4b5df014e24277d1191e0ee73405e4
Ashuka Xue <axue@vmware.com>
2023-07-21 17:40:25 -0700

    update so that maven is pulled from google cloud storage instead of the apache site

---
+++ concourse/scripts/download-maven-from-apache-mirror.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+
+PXF_HOME=${HOME}/workspace/pxf/downloads/
+
+MAVEN_VERSION="${1:?a Maven version must be provided}"
+
+maven_dist="apache-maven-${MAVEN_VERSION}-bin.tar.gz"
+maven_path="maven/maven-3/${MAVEN_VERSION}/binaries"
+
+response_json="$(curl -fsSL "https://www.apache.org/dyn/closer.lua/${maven_path}?as_json")"
+in_dist="$(jq -r '.in_dist // false' <<<"$response_json")"
+
+if [ "$in_dist" == "false" ]; then
+    echo >&2 "${maven_dist} was not found in dist; attempting to download from archive.apache.org"
+    download_url="https://archive.apache.org/dist/${maven_path}/${maven_dist}"
+else
+    preferred="$(jq -r ".preferred" <<<"$response_json")"
+    path_info="$(jq -r ".path_info" <<<"$response_json")"
+    if [ -z "$preferred" ] || [ -z "$path_info" ]; then
+        echo >&2 "unable to get download URL from response"
+        echo >&2 "$response_json"
+        exit 1
+    fi
+    download_url="${preferred%/}/$path_info"
+fi
+
+preferred="$(jq -r ".preferred" <<<"$response_json")"
+path_info="$(jq -r ".path_info" <<<"$response_json")"
+
+if [ -z "$preferred" ] || [ -z "$path_info" ]; then
+    echo >&2 "unable to get download URL from response"
+    echo >&2 "$response_json"
+    exit 1
+fi
+
+echo "Downloading $download_url to ${PXF_HOME}/${maven_dist}..."
+curl -Lo ${PXF_HOME}/${maven_dist} "$download_url"
