768a075ed42b3280945943e7a31559178d033b7e
Ashuka Xue <axue@vmware.com>
2023-07-24 12:31:54 -0700

    fixup download maven script

--- concourse/scripts/download-maven-from-apache-mirror.sh
+++ concourse/scripts/download-maven-from-apache-mirror.sh
@@ -8,14 +8,14 @@ PXF_HOME=${HOME}/workspace/pxf/downloads/
 MAVEN_VERSION="${1:?a Maven version must be provided}"
 
 maven_dist="apache-maven-${MAVEN_VERSION}-bin.tar.gz"
-maven_path="maven/maven-3/${MAVEN_VERSION}/binaries"
+maven_full_path="maven/maven-3/${MAVEN_VERSION}/binaries/${maven_dist}"
 
-response_json="$(curl -fsSL "https://www.apache.org/dyn/closer.lua/${maven_path}?as_json")"
+response_json="$(curl -fsSL "https://www.apache.org/dyn/closer.lua/${maven_full_path}?as_json")"
 in_dist="$(jq -r '.in_dist // false' <<<"$response_json")"
 
 if [ "$in_dist" == "false" ]; then
     echo >&2 "${maven_dist} was not found in dist; attempting to download from archive.apache.org"
-    download_url="https://archive.apache.org/dist/${maven_path}/${maven_dist}"
+    download_url="https://archive.apache.org/dist/${maven_full_path}"
 else
     preferred="$(jq -r ".preferred" <<<"$response_json")"
     path_info="$(jq -r ".path_info" <<<"$response_json")"
