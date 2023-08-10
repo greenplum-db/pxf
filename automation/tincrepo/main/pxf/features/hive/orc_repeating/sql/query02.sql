4be6da090d66c8603f96b7938c56ba702946ee3e
Bradford D. Boyle <bradfordb@vmware.com>
2023-08-09 18:09:34 -0700

    fixup! wip: updated expected output

--- automation/tincrepo/main/pxf/features/hive/orc_repeating/sql/query02.sql
+++ automation/tincrepo/main/pxf/features/hive/orc_repeating/sql/query02.sql
@@ -1,4 +1,5 @@
 -- @description query01 for PXF Hive ORC vectorized with repeating data cases
+SET datestyle TO 'ISO, MDY';
 \pset null 'NIL'
 
 SELECT * FROM pxf_hive_orc_vectorize_repeating_no_nulls ORDER BY t1;
