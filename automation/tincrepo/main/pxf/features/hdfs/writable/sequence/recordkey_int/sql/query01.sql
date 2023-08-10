1c40c597a7aa406dbb5adfb44b7b4d41aa7da85a
Bradford D. Boyle <bradfordb@vmware.com>
2023-08-10 14:09:20 -0700

    fixup! wip: updated expected output

--- automation/tincrepo/main/pxf/features/hdfs/writable/sequence/recordkey_int/sql/query01.sql
+++ automation/tincrepo/main/pxf/features/hdfs/writable/sequence/recordkey_int/sql/query01.sql
@@ -1,3 +1,4 @@
 -- @description query01 for PXF HDFS Readable Sequence with int recordkey 
+SET datestyle TO 'ISO, MDY';
 
 SELECT * from pxf_recordkey_int_type_r ORDER BY num1;
--- automation/tincrepo/main/pxf/features/hdfs/writable/sequence/recordkey_text/sql/query01.sql
+++ automation/tincrepo/main/pxf/features/hdfs/writable/sequence/recordkey_text/sql/query01.sql
@@ -1,3 +1,4 @@
 -- @description query01 for PXF HDFS Readable Sequence with text recordkey 
+SET datestyle TO 'ISO, MDY';
 
 SELECT * from pxf_recordkey_text_type_r ORDER BY num1;
