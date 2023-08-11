c4bdf3b029478487a7c02fd027112e7038047c27
Bradford D. Boyle <bradfordb@vmware.com>
2023-08-10 23:19:11 -0700

    fixup! wip: updated expected output

--- automation/tincrepo/main/pxf/features/hdfs/readable/json/malformed_record_with_reject_limit_csv/sql/query01.sql
+++ automation/tincrepo/main/pxf/features/hdfs/readable/json/malformed_record_with_reject_limit_csv/sql/query01.sql
@@ -3,6 +3,8 @@
 -- s/( +):/\1|/
 -- m/\+$/
 -- s/\+$//
+-- m/^NOTICE:  found \d+ data formatting errors.*/
+-- s/.*/NOTICE:  Found 1 data formatting errors (1 or more input rows). Rejected related input data./
 -- end_matchsubs
 -- @description query01 for PXF HDFS Readable Json with malformed record test cases
 SELECT *
--- automation/tincrepo/main/pxf/features/hdfs/readable/json/mismatched_types_with_reject_limit/sql/query01.sql
+++ automation/tincrepo/main/pxf/features/hdfs/readable/json/mismatched_types_with_reject_limit/sql/query01.sql
@@ -1,4 +1,8 @@
 -- @description query01 for PXF HDFS Readable Json supported primitive types test cases
+-- start_matchsubs
+-- m/^NOTICE:  found \d+ data formatting errors.*/
+-- s/.*/NOTICE:  Found 6 data formatting errors (6 or more input rows). Rejected related input data./
+-- end_matchsubs
 SELECT *
 FROM jsontest_mismatched_types_with_reject_limit
 ORDER BY type_int;
