bc64e32d0c8ef80271f84b6351f6778e9cff238c
Bradford D. Boyle <bradfordb@vmware.com>
2023-08-10 18:36:30 -0700

    fixup! wip: updated expected output

--- automation/tincrepo/main/pxf/features/jdbc/hive/sql/query01.sql
+++ automation/tincrepo/main/pxf/features/jdbc/hive/sql/query01.sql
@@ -1,2 +1,3 @@
 -- @description query01 for JDBC Hive query without partitioning
+SET datestyle TO 'ISO, MDY';
 SELECT s1, s2, n1, d1, round(dc1, 18), tm, f, bg, b, tn, sml, dt, vc1, c1 FROM pxf_jdbc_hive_types_table ORDER BY s1;
