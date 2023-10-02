14b1024dbda11fa47f7d9358b8b1e2c7de7f2ce8
Ashuka Xue <axue@vmware.com>
2023-10-02 12:39:41 -0700

    Fixup HiveAdvancedTest now that we don't need ambari
    this works locally since the user typically owns the singlecluster
    directory.
    
    this does not work in ci as the singlecluster directory is owned by
    root. we cannot start up the hive service again as we do not have the
    permissions to do so when running this test in ci

--- automation/src/test/java/org/greenplum/pxf/automation/features/hive/HiveAdvancedTest.java
+++ automation/src/test/java/org/greenplum/pxf/automation/features/hive/HiveAdvancedTest.java
@@ -1,11 +1,10 @@
 package org.greenplum.pxf.automation.features.hive;
 
-import annotations.ExpectedFailure;
-
 import org.greenplum.pxf.automation.AdvancedFunctionality;
 import org.greenplum.pxf.automation.components.cluster.PhdCluster;
 import org.greenplum.pxf.automation.structures.tables.hive.HiveTable;
 import org.greenplum.pxf.automation.structures.tables.utils.TableFactory;
+import org.testng.annotations.Test;
 
 /**
  * Tests Hive plugin in PXF
@@ -14,19 +13,17 @@ public class HiveAdvancedTest extends AdvancedFunctionality {
 
     @Override
     protected void afterClass() throws Exception {
+        cluster.stop(PhdCluster.EnumClusterServices.hive);
         cluster.start(PhdCluster.EnumClusterServices.hive);
     }
 
     /**
      * Stop Hive metastore, run query and expect error message, and restart Hive
      * metastore.
-     * <p>
-     * TODO: enable test when issue is resolved.
      *
      * @throws Exception if test fails to run
      */
-    // @Test(groups = {"hive", "features", "gpdb", "security"})
-    @ExpectedFailure(reason = "[#91052648] - Can't start/stop Hive MetaStore in Ambari setup.")
+     @Test(groups = {"hive", "features", "gpdb", "security"})
     public void hiveMetastoreDown() throws Exception {
         HiveTable hiveTable = new HiveTable("hive_table", null);
         exTable = TableFactory.getPxfHiveReadableTable(
